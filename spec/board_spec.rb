adcb23c432da84080a9afee2bd5f7f000ec28258
Carl Gudmundsson <komoss@gmail.com>
2022-11-20 20:55:14 +0100

    Add Board class

---
+++ spec/board_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative '../lib/board'
+
+describe Board do
+
+  describe '#get_piece' do
+    # returns the piece at a specific coordinate
+  end
+
+  describe '#reset!' do
+    # Method that sets all pieces to initial position
+  end
+
+end
