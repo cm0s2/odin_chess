4b109fa270e5f20da1c4a23374efbee22b8d18c2
Carl Gudmundsson <komoss@gmail.com>
2022-11-21 13:44:30 +0100

    Start implementing reset

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
+require_relative 'square'
+
 class Board
   def initialize
-    
+    @grid = Array.new(8) { Array.new(8) { Square.new }}
+  end
+
+  def reset!
+    # White
+    @grid.each do |file|
+      file[1].piece = Pawn.new(false)
+      file[6].piece = Pawn.new(true)
+    end
+
+    # Black
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,6 +2,8 @@
 
 require_relative '../lib/board'
 
+require_relative '../lib/Pawn'
+
 describe Board do
 
   describe '#get_piece' do
@@ -9,7 +11,17 @@ describe Board do
   end
 
   describe '#reset!' do
+    let(:empty_board) { described_class.new }
+    let(:grid) { empty_board.instance_variable_get(:@grid) }
     # Method that sets all pieces to initial position
+
+
+    it 'populates the board with pieces' do
+      empty_board.reset!
+      piece = grid[0][6].piece
+      p piece
+      expect(piece).to be_an_instance_of(Pawn)
+    end
   end
 
 end
